c543a45d427601df632ae9ea876854f8ec104687
Piyushyadav0417 <146307918+Piyushyadav0417@users.noreply.github.com>
2024-05-13 01:46:55 +0530

    Add files via upload

---
+++ allvariables.py
@@ -0,0 +1,56 @@
+#1Numeric variable
+a = 5  #this is a interger variable
+print(5)
+price = 99.91#this is a float variable
+print(price)
+
+
+
+
+#2String Variables
+name = "JOhn"
+print(name)
+
+
+
+
+#Boolean  Variables
+are_you_18 = True
+print(are_you_18)
+
+
+
+
+#None Variable 
+Do_You_Have_Money = None
+print(Do_You_Have_Money)
+
+
+
+
+#List Variables
+Fruits = ["Orange", "Banana", "Apple", "Graps", ]
+print(Fruits)
+
+
+
+
+
+#Tuple Variables
+Numbers = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+print(Numbers)
+
+
+
+
+#Dictionary Variables
+Person = {'name': 'john', 'age': 31}
+ 
+print(Person)
+
+
+
+
+#Set Variables 
+Unique_Number = {12, 24, 423}
+print(Unique_Number)
